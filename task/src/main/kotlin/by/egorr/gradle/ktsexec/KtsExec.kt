66e075fa60de0bce63ec1b96005a8045f6065f47
Yahor Berdnikau <egorr.berd@gmail.com>
2019-03-30 21:16:20 +0100

    Change KtxExec script param type to RegularFileProperty.

--- task/src/main/kotlin/by/egorr/gradle/ktsexec/KtsExec.kt
+++ task/src/main/kotlin/by/egorr/gradle/ktsexec/KtsExec.kt
@@ -1,16 +1,20 @@
 package by.egorr.gradle.ktsexec
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.TaskExecutionException
+import java.io.IOException
 
 const val KTS_EXEC_CONFIGURATION_NAME = "ktsExecConfiguration"
 private const val HOST_VERSION = "1.1.0-SNAPSHOT"
 private const val KOTLIN_VERSION = "1.3.21"
 
+@Suppress("UnstableApiUsage")
 open class KtsExec : DefaultTask() {
     @field:Input
-    var script: String? = null
+    val script: RegularFileProperty = project.objects.fileProperty()
 
     init {
         project.configurations.maybeCreate(KTS_EXEC_CONFIGURATION_NAME)
@@ -36,13 +40,17 @@ open class KtsExec : DefaultTask() {
 
     @TaskAction
     fun runScript() {
-        val scriptFile = script
-        if (scriptFile != null) {
-            project.javaexec {
-                it.classpath = project.configurations.getByName(KTS_EXEC_CONFIGURATION_NAME)
-                it.main = "by.egorr.gradle.ktsexec.host.KtsExecKotlinScriptHost"
-                it.args = listOf(scriptFile)
-                it.standardInput = System.`in`
+        if (script.isPresent) {
+            val scriptFile = script.get().asFile
+            if (scriptFile.exists()) {
+                project.javaexec {
+                    it.classpath = project.configurations.getByName(KTS_EXEC_CONFIGURATION_NAME)
+                    it.main = "by.egorr.gradle.ktsexec.host.KtsExecKotlinScriptHost"
+                    it.args = listOf(scriptFile.absolutePath)
+                    it.standardInput = System.`in`
+                }
+            } else {
+                throw TaskExecutionException(this, IOException("Provided script file $scriptFile is not available"))
             }
         } else {
             logger.warn("No script file was provided!")
