89ca37706bb3cbb70bcd6224a9f865e74fecba59
Yahor Berdnikau <egorr.berd@gmail.com>
2019-05-04 19:27:49 +0200

    Update ktlint-gradle to 7.4.0 version.

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -1,6 +1,6 @@
 object Versions {
     val kotlin = "1.3.31"
-    val ktlintGradle = "7.2.1"
+    val ktlintGradle = "7.4.0"
     val gradleWrapper = "5.4.1"
     val bintray = "1.8.4"
     val gradlePluginPublish = "0.10.1"
